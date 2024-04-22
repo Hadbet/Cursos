1f3347ce96083bc6a61ddb9ac3bbe56d7019e19e
hadbet <hadbet.altamirano@grammer.com>
2024-04-22 14:43:29 -0600

    Ingreso de nombres de curso

--- dao/consultaHorario.php
+++ dao/consultaHorario.php
@@ -9,7 +9,7 @@ function ContadorApu()
     $con = new LocalConector();
     $conex = $con->conectar();
 
-    $datos = mysqli_query($conex, "SELECT * FROM `HorarioCurso`;");
+    $datos = mysqli_query($conex, "SELECT * FROM `Horario_Curso`;");
 
     $resultado = mysqli_fetch_all($datos, MYSQLI_ASSOC);
     echo json_encode(array("data" => $resultado));
