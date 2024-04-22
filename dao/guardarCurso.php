4a5e8e1368b5d816033a90cf1b73f349698cdd2f
hadbet <hadbet.altamirano@grammer.com>
2024-04-22 16:21:05 -0600

    Ingreso de nombres de curso

--- dao/guardarCurso.php
+++ dao/guardarCurso.php
@@ -4,14 +4,15 @@ include_once('db/db_RH.php');
 $Nombre=$_POST['nombre'];
 $Horario=$_POST['horario'];
 $Fecha=$_POST['fecha'];
+$Capacidad=$_POST['capacidad'];
 
-registroUsu($Nombre,$Horario,$Fecha);
-function registroUsu($Nombre,$Horario,$Fecha){
+registroUsu($Nombre,$Horario,$Fecha,$Capacidad);
+function registroUsu($Nombre,$Horario,$Fecha,$Capacidad){
 
     $con = new LocalConector();
     $conex=$con->conectar();
 
-    $insertRegistro= "INSERT INTO `Cursos`(`NombreCurso`, `Horario`, `Fecha`, `Estatus`) VALUES ('$Nombre','$Horario','$Fecha',1)";
+    $insertRegistro= "INSERT INTO `Cursos`(`NombreCurso`, `Horario`, `Fecha`, `Estatus`, `Capacidad`) VALUES ('$Nombre','$Horario','$Fecha',1,$Capacidad)";
 
     $rsinsertUsu=mysqli_query($conex,$insertRegistro);
     mysqli_close($conex);
