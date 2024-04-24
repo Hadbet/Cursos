9f2d5b81c6b97f48e0c8aa59002d5d5daf71761d
hadbet <hadbet.altamirano@grammer.com>
2024-04-24 11:01:20 -0600

    Ingreso de nombres de curso

--- dao/consultaCursoAsistencia.php
+++ dao/consultaCursoAsistencia.php
@@ -3,21 +3,17 @@
 include_once('db/db_RH.php');
 
 $Horario=$_GET['horario'];
+$Curso=$_GET['curso'];
+$Fecha=$_GET['fecha'];
 
-ContadorApu($Horario);
+ContadorApu($Horario,$Curso,$Fecha);
 
-function ContadorApu($Horario)
+function ContadorApu($Horario,$Curso,$Fecha)
 {
     $con = new LocalConector();
     $conex = $con->conectar();
 
-    $horarioParts = explode(", ", $Horario);
-
-    $Curso = trim($horarioParts[0]);
-    $Fecha = trim($horarioParts[1]);
-    $HorarioCurso = trim($horarioParts[2]);
-
-    $datos = mysqli_query($conex, "SELECT * FROM `BitacoraCursos` WHERE `Curso`= '$Curso' and `Horario` = '$HorarioCurso' and `Fecha` = '$Fecha';");
+    $datos = mysqli_query($conex, "SELECT * FROM `BitacoraCursos` WHERE `Curso`= '$Curso' and `Horario` = '$Horario' and `Fecha` = '$Fecha';");
 
     $resultado = mysqli_fetch_all($datos, MYSQLI_ASSOC);
     echo json_encode(array("data" => $resultado));
