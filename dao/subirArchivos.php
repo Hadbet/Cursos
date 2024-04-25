4be88ae0fa948717d36fa7a9015b7597f986ccee
hadbet <hadbet.altamirano@grammer.com>
2024-04-25 11:06:11 -0600

    Ingreso de nombres de curso

--- dao/subirArchivos.php
+++ dao/subirArchivos.php
@@ -1,11 +1,20 @@
 <?php
 $target_dir = "documentacion/"; // especifica el directorio donde se subirá el archivo
-$target_file = $target_dir . basename($_FILES["file"]["name"]); // especifica la ruta del archivo a subir
 
-// intenta subir el archivo
-if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)) {
-    echo "El archivo ". basename( $_FILES["file"]["name"]). " ha sido subido.";
+// verifica si se subieron archivos
+if (!empty($_FILES['archivos']['name'][0])) {
+    // recorre cada archivo
+    for ($i = 0; $i < count($_FILES['archivos']['name']); $i++) {
+        $target_file = $target_dir . basename($_FILES["archivos"]["name"][$i]); // especifica la ruta del archivo a subir
+
+        // intenta subir el archivo
+        if (move_uploaded_file($_FILES["archivos"]["tmp_name"][$i], $target_file)) {
+            echo "El archivo ". basename( $_FILES["archivos"]["name"][$i]). " ha sido subido.";
+        } else {
+            echo "Lo siento, hubo un error subiendo el archivo ". basename( $_FILES["archivos"]["name"][$i]). ".";
+        }
+    }
 } else {
-    echo "Lo siento, hubo un error subiendo tu archivo.";
+    echo "No se subieron archivos.";
 }
 ?>
