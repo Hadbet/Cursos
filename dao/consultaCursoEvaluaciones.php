534f9061815f2f91d56d82ac2a393743cfa8e06a
hadbet <hadbet.altamirano@grammer.com>
2024-04-29 11:13:01 -0600

    Ingreso de nombres de curso

--- dao/consultaCursoEvaluaciones.php
+++ dao/consultaCursoEvaluaciones.php
@@ -13,7 +13,7 @@ function ContadorApu($Horario,$Curso,$Fecha)
     $con = new LocalConector();
     $conex = $con->conectar();
 
-    $datos = mysqli_query($conex, "SELECT * FROM `BitacoraCursos` WHERE `EstatusAsistencia` = 1 and `Horario` = '$Horario' and `Fecha` = '$Fecha' and `Curso` = '$Curso' order by `Curso`,`Horario`,`Fecha`;");
+    $datos = mysqli_query($conex, "SELECT * FROM `BitacoraCursos` WHERE (`EstatusAsistencia` = 1 or `EstatusAsistencia` = 2) and `Horario` = '$Horario' and `Fecha` = '$Fecha' and `Curso` = '$Curso' order by `Curso`,`Horario`,`Fecha`;");
 
     $resultado = mysqli_fetch_all($datos, MYSQLI_ASSOC);
     echo json_encode(array("data" => $resultado));
