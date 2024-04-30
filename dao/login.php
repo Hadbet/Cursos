bd116c36c82ddfb3afec7c076e99644ebbad4fae
hadbet <hadbet.altamirano@grammer.com>
2024-04-30 11:28:52 -0600

    Ingreso de nombres de curso

--- dao/login.php
+++ dao/login.php
@@ -3,29 +3,29 @@
 require 'consultaEmpleado.php';
 
 if(isset($_POST['btnSolicitante'])){
+    /*
+        session_start();
+        $Nomina = $_POST['nomina'];
+        $Password=$_POST['password'];
 
-    session_start();
-    $Nomina = $_POST['nomina'];
-    $Password=$_POST['password'];
+        if (strlen($Nomina) == 1) { $Nomina = "0000000".$Nomina; }
+        if (strlen($Nomina) == 2) { $Nomina = "000000".$Nomina; }
+        if (strlen($Nomina) == 3) { $Nomina = "00000".$Nomina; }
+        if (strlen($Nomina) == 4) { $Nomina = "0000".$Nomina; }
+        if (strlen($Nomina) == 5) { $Nomina = "000".$Nomina; }
+        if (strlen($Nomina) == 6) { $Nomina = "00".$Nomina; }
+        if (strlen($Nomina) == 7) { $Nomina = "0".$Nomina; }
 
-    if (strlen($Nomina) == 1) { $Nomina = "0000000".$Nomina; }
-    if (strlen($Nomina) == 2) { $Nomina = "000000".$Nomina; }
-    if (strlen($Nomina) == 3) { $Nomina = "00000".$Nomina; }
-    if (strlen($Nomina) == 4) { $Nomina = "0000".$Nomina; }
-    if (strlen($Nomina) == 5) { $Nomina = "000".$Nomina; }
-    if (strlen($Nomina) == 6) { $Nomina = "00".$Nomina; }
-    if (strlen($Nomina) == 7) { $Nomina = "0".$Nomina; }
-/*
-    $statusLogin = verificacionUsuario($Nomina, $Password);
+        $statusLogin = verificacionUsuario($Nomina, $Password);
 
-    if($statusLogin == 1){
-        $_SESSION['nomina'] = $Nomina;
-        $_SESSION['password'] = $Password;
-        echo "<META HTTP-EQUIV='REFRESH' CONTENT='1; URL=../inicio.html'>";
-    }else if($statusLogin == 0){
-        echo "<script>alert('Acceso Denegado')</script>";
-        echo "<META HTTP-EQUIV='REFRESH' CONTENT='1; URL=../index.html'>";
-    }*/
+        if($statusLogin == 1){
+            $_SESSION['nomina'] = $Nomina;
+            $_SESSION['password'] = $Password;
+            echo "<META HTTP-EQUIV='REFRESH' CONTENT='1; URL=../inicio.html'>";
+        }else if($statusLogin == 0){
+            echo "<script>alert('Acceso Denegado')</script>";
+            echo "<META HTTP-EQUIV='REFRESH' CONTENT='1; URL=../index.html'>";
+        }*/
 
 }
 
