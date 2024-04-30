a0bd834aaf266d1ebaef3add8c0117ec2c92c953
hadbet <hadbet.altamirano@grammer.com>
2024-04-30 11:22:59 -0600

    Ingreso de nombres de curso

--- dao/login.php
+++ dao/login.php
@@ -15,7 +15,7 @@ if(isset($_POST['btnSolicitante'])){
     if (strlen($Nomina) == 5) { $Nomina = "000".$Nomina; }
     if (strlen($Nomina) == 6) { $Nomina = "00".$Nomina; }
     if (strlen($Nomina) == 7) { $Nomina = "0".$Nomina; }
-
+/*
     $statusLogin = verificacionUsuario($Nomina, $Password);
 
     if($statusLogin == 1){
@@ -26,7 +26,7 @@ if(isset($_POST['btnSolicitante'])){
         echo "<script>alert('Acceso Denegado')</script>";
         echo "<META HTTP-EQUIV='REFRESH' CONTENT='1; URL=../index.html'>";
     }
-
+*/
 }
 
 if(isset($_POST['btnSalir'])){
