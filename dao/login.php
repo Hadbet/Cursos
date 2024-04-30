b5286131f1e40bc9e5baed64e8951b4b0aa03cff
hadbet <hadbet.altamirano@grammer.com>
2024-04-30 11:34:15 -0600

    Ingreso de nombres de curso

--- dao/login.php
+++ dao/login.php
@@ -2,42 +2,41 @@
 
 require 'consultaEmpleado.php';
 
-if(isset($_POST['btnSolicitante'])){
+session_start();
+$Nomina = $_POST['nomina'];
+$Password = $_POST['password'];
 
-        session_start();
-        $Nomina = $_POST['nomina'];
-        $Password=$_POST['password'];
-
-        if (strlen($Nomina) == 1) { $Nomina = "0000000".$Nomina; }
-        if (strlen($Nomina) == 2) { $Nomina = "000000".$Nomina; }
-        if (strlen($Nomina) == 3) { $Nomina = "00000".$Nomina; }
-        if (strlen($Nomina) == 4) { $Nomina = "0000".$Nomina; }
-        if (strlen($Nomina) == 5) { $Nomina = "000".$Nomina; }
-        if (strlen($Nomina) == 6) { $Nomina = "00".$Nomina; }
-        if (strlen($Nomina) == 7) { $Nomina = "0".$Nomina; }
-
-        $statusLogin = verificacionUsuario($Nomina, $Password);
-
-        if($statusLogin == 1){
-            $_SESSION['nomina'] = $Nomina;
-            $_SESSION['password'] = $Password;
-            echo "<META HTTP-EQUIV='REFRESH' CONTENT='1; URL=../inicio.html'>";
-        }else if($statusLogin == 0){
-            echo "<script>alert('Acceso Denegado')</script>";
-            echo "<META HTTP-EQUIV='REFRESH' CONTENT='1; URL=../index.html'>";
-        }
-
+if (strlen($Nomina) == 1) {
+    $Nomina = "0000000" . $Nomina;
+}
+if (strlen($Nomina) == 2) {
+    $Nomina = "000000" . $Nomina;
+}
+if (strlen($Nomina) == 3) {
+    $Nomina = "00000" . $Nomina;
+}
+if (strlen($Nomina) == 4) {
+    $Nomina = "0000" . $Nomina;
+}
+if (strlen($Nomina) == 5) {
+    $Nomina = "000" . $Nomina;
+}
+if (strlen($Nomina) == 6) {
+    $Nomina = "00" . $Nomina;
+}
+if (strlen($Nomina) == 7) {
+    $Nomina = "0" . $Nomina;
 }
 
-if(isset($_POST['btnSalir'])){
-    session_start();
-    session_destroy();
-    echo "<script>alert('Acceso Correcto')</script>";
-    echo "<META HTTP-EQUIV='REFRESH' CONTENT='1; URL=../Historico.html'>";
-}
+$statusLogin = verificacionUsuario($Nomina, $Password);
 
-if(isset($_POST['btnRegistro'])){
-    echo "<script>alert('Acceso Correcto')</script>";
+if ($statusLogin == 1) {
+    $_SESSION['nomina'] = $Nomina;
+    $_SESSION['password'] = $Password;
+    echo "<META HTTP-EQUIV='REFRESH' CONTENT='1; URL=../inicio.html'>";
+} else if ($statusLogin == 0) {
+    echo "<script>alert('Acceso Denegado')</script>";
+    echo "<META HTTP-EQUIV='REFRESH' CONTENT='1; URL=../index.html'>";
 }
 
 
