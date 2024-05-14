c58de2ac6bf979967ae0a0bdbd6e13ff18fa343f
hadbet <hadbet.altamirano@grammer.com>
2024-05-14 17:55:19 -0600

    Ingreso de nombres de curso

--- dao/consultaApuEficiencia.php
+++ dao/consultaApuEficiencia.php
@@ -8,7 +8,7 @@ function ContadorApu()
     $con = new LocalConector();
     $conex = $con->conectar();
 
-    $datos = mysqli_query($conex, "SELECT APU, SUM(CASE WHEN EstatusAsistencia = 2 AND Evaluacion >= 6 THEN 1 ELSE 0 END) as Aprobados, SUM(CASE WHEN EstatusAsistencia = 2 AND Evaluacion < 6 THEN 1 ELSE 0 END) as Reprobados FROM BitacoraCursos GROUP BY APU;");
+    $datos = mysqli_query($conex, "SELECT APU, SUM(CASE WHEN EstatusAsistencia >= 2 AND Evaluacion >= 6 THEN 1 ELSE 0 END) as Aprobados, SUM(CASE WHEN EstatusAsistencia >= 2 AND Evaluacion < 6 THEN 1 ELSE 0 END) as Reprobados FROM BitacoraCursos GROUP BY APU;");
 
     $resultado = mysqli_fetch_all($datos, MYSQLI_ASSOC);
     echo json_encode(array("data" => $resultado));
