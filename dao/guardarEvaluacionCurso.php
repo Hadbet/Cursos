e7ffe8bef6506108f0630108781d8590b73858be
hadbet <hadbet.altamirano@grammer.com>
2024-05-14 17:14:59 -0600

    Ingreso de nombres de curso

--- dao/guardarEvaluacionCurso.php
+++ dao/guardarEvaluacionCurso.php
@@ -10,9 +10,19 @@ $Comentarios=$_POST['comentarios'];
 $Calificacion=$_POST['calificacion'];
 $Id=$_POST['id'];
 
+$ResOne=$_POST['resOne'];
+$ResTwo=$_POST['resTwo'];
+$ResThree=$_POST['resThree'];
+$ResFour=$_POST['resFour'];
+$ResFive=$_POST['resFive'];
+$ResSix=$_POST['resSix'];
+$ResSeven=$_POST['resSeven'];
+$ResEight=$_POST['resEight'];
+$ResNine=$_POST['resNine'];
+$ComentariosInstructor=$_POST['comentariosInstructor'];
 
-registroUsu($Nombre,$Nomina,$Horario,$Fecha,$Curso,$Comentarios,$Calificacion,$Id);
-function registroUsu($Nombre,$Nomina,$Horario,$Fecha,$Curso,$Comentarios,$Calificacion, $Id){
+registroUsu($Nombre,$Nomina,$Horario,$Fecha,$Curso,$Comentarios,$Calificacion,$Id,$ResOne,$ResTwo,$ResThree,$ResFour,$ResFive,$ResSix,$ResSeven,$ResEight,$ResNine,$ComentariosInstructor);
+function registroUsu($Nombre,$Nomina,$Horario,$Fecha,$Curso,$Comentarios,$Calificacion, $Id,$ResOne,$ResTwo,$ResThree,$ResFour,$ResFive,$ResSix,$ResSeven,$ResEight,$ResNine,$ComentariosInstructor){
 
     $con = new LocalConector();
     $conex=$con->conectar();
@@ -31,8 +41,24 @@ function registroUsu($Nombre,$Nomina,$Horario,$Fecha,$Curso,$Comentarios,$Califi
         return 0;
     }
 
+    $Respuestas = $ResOne."_".$ResTwo."_".$ResThree."_".$ResFour."_".$ResFive."_".$ResSix."_".$ResSeven."_".$ResEight."_".$ResNine."_".$ComentariosInstructor;
+
+    $ResOne = convertirRespuestaANumero($ResOne);
+    $ResTwo = convertirRespuestaANumero($ResTwo);
+    $ResThree = convertirRespuestaANumero($ResThree);
+    $ResFour = convertirRespuestaANumero($ResFour);
+    $ResFive = convertirRespuestaANumero($ResFive);
+    $ResSix = convertirRespuestaANumero($ResSix);
+    $ResSeven = convertirRespuestaANumero($ResSeven);
+    $ResEight = convertirRespuestaANumero($ResEight);
+    $ResNine = convertirRespuestaANumero($ResNine);
+
+    $suma = $ResOne + $ResTwo + $ResThree + $ResFour + $ResFive + $ResSix + $ResSeven + $ResEight + $ResNine;
+
+    $promedio = $suma / 9;
+
     // Si la actualización fue exitosa, realiza la inserción
-    $insertRegistro= "INSERT INTO `Evaluacion_Curso`(`Curso`, `Fecha`, `Horario`, `Nomina`, `Nombre`, `Calificacion`, `Comentarios`,`FechaEvaluacion`) VALUES ('$Curso','$Fecha','$Horario','$Nomina','$Nombre','$Calificacion','$Comentarios','$DateAndTime')";
+    $insertRegistro= "INSERT INTO `Evaluacion_Curso`(`Curso`, `Fecha`, `Horario`, `Nomina`, `Nombre`, `Calificacion`, `Comentarios`,`FechaEvaluacion`,`Respuestas`) VALUES ('$Curso','$Fecha','$Horario','$Nomina','$Nombre','$promedio','$Comentarios','$DateAndTime','$Respuestas')";
 
     $rsinsertUsu=mysqli_query($conex,$insertRegistro);
     mysqli_close($conex);
@@ -41,6 +67,22 @@ function registroUsu($Nombre,$Nomina,$Horario,$Fecha,$Curso,$Comentarios,$Califi
         echo "0";
     }else{
         return 1;
+    }
+}
+
+function convertirRespuestaANumero($respuesta) {
+    $partes = explode('-', $respuesta);
+    $calificacion = trim($partes[1]);
+
+    switch ($calificacion) {
+        case 'Mal':
+            return 0;
+        case 'Bien':
+            return 0.5;
+        case 'Excelente':
+            return 1;
+        default:
+            return 0;
     }
 }
 
