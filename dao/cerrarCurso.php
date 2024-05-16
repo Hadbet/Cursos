aab3ddca1e8b7622fed2647c337ebaebaa59d07b
hadbet <hadbet.altamirano@grammer.com>
2024-05-16 09:49:07 -0600

    Ingreso de nombres de curso

---
+++ dao/cerrarCurso.php
@@ -0,0 +1,30 @@
+<?php
+include_once('db/db_RH.php');
+
+$Horario=$_POST['horario'];
+
+registroUsu($Horario);
+function registroUsu($Horario){
+
+    $con = new LocalConector();
+    $conex=$con->conectar();
+
+    $horarioParts = explode(", ", $Horario);
+
+    $Curso = trim($horarioParts[0]);
+    $Fecha = trim($horarioParts[1]);
+    $HorarioCurso = trim($horarioParts[2]);
+
+    $insertRegistro= "UPDATE `Cursos` SET `Estatus` = 2 WHERE `Horario` = '$HorarioCurso' and `Fecha` = '$Fecha' and `NombreCurso` = '$Curso'";
+
+    $rsinsertUsu=mysqli_query($conex,$insertRegistro);
+    mysqli_close($conex);
+
+    if(!$rsinsertUsu){
+        echo "0";
+    }else{
+        return 1;
+    }
+}
+
+?>
