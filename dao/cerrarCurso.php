de37fa44423409626036651453bd196f9990dad7
hadbet <hadbet.altamirano@grammer.com>
2024-05-16 10:22:07 -0600

    Ingreso de nombres de curso

--- dao/cerrarCurso.php
+++ dao/cerrarCurso.php
@@ -9,13 +9,17 @@ function registroUsu($Horario){
     $con = new LocalConector();
     $conex=$con->conectar();
 
+    $Object = new DateTime();
+    $Object->setTimezone(new DateTimeZone('America/Denver'));
+    $DateAndTime = $Object->format("Y/m/d");
+
     $horarioParts = explode(", ", $Horario);
 
     $Curso = trim($horarioParts[0]);
     $Fecha = trim($horarioParts[1]);
     $HorarioCurso = trim($horarioParts[2]);
 
-    $insertRegistro= "UPDATE `Cursos` SET `Estatus` = 2 WHERE `Horario` = '$HorarioCurso' and `Fecha` = '$Fecha' and `NombreCurso` = '$Curso'";
+    $insertRegistro= "UPDATE `Cursos` SET `Estatus` = 2,`FechaTermino`= '$DateAndTime' WHERE `Horario` = '$HorarioCurso' and `Fecha` = '$Fecha' and `NombreCurso` = '$Curso'";
 
     $rsinsertUsu=mysqli_query($conex,$insertRegistro);
     mysqli_close($conex);
