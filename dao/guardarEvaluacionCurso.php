f90e6c6f5cece103c1e4e4984957a1823d70406a
hadbet <hadbet.altamirano@grammer.com>
2024-05-16 09:27:43 -0600

    Ingreso de nombres de curso

--- dao/guardarEvaluacionCurso.php
+++ dao/guardarEvaluacionCurso.php
@@ -55,7 +55,7 @@ function registroUsu($Nombre,$Nomina,$Horario,$Fecha,$Curso,$Comentarios,$Califi
 
     $suma = $ResOne + $ResTwo + $ResThree + $ResFour + $ResFive + $ResSix + $ResSeven + $ResEight + $ResNine;
 
-    $promedio = ($suma * 100) / 9;
+    $promedio = round(($suma * 100) / 9, 2);
 
     // Si la actualización fue exitosa, realiza la inserción
     $insertRegistro= "INSERT INTO `Evaluacion_Curso`(`Curso`, `Fecha`, `Horario`, `Nomina`, `Nombre`, `Calificacion`, `Comentarios`,`FechaEvaluacion`,`Respuestas`,`ComentarioInstructor`) VALUES ('$Curso','$Fecha','$Horario','$Nomina','$Nombre','$promedio','$Comentarios','$DateAndTime','$Respuestas','$ComentariosInstructor')";
