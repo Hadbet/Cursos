ead22f73e43b5187f12f976d4d905449ec6d13ac
hadbet <hadbet.altamirano@grammer.com>
2024-05-16 09:42:00 -0600

    Ingreso de nombres de curso

--- dao/consultaCursoActuales.php
+++ dao/consultaCursoActuales.php
@@ -9,7 +9,7 @@ function ContadorApu()
     $con = new LocalConector();
     $conex = $con->conectar();
 
-    $datos = mysqli_query($conex, "SELECT * FROM `Cursos` WHERE `Fecha` >= CURDATE();");
+    $datos = mysqli_query($conex, "SELECT * FROM `Cursos` WHERE `Estatus` =1;");
 
     $resultado = mysqli_fetch_all($datos, MYSQLI_ASSOC);
     echo json_encode(array("data" => $resultado));
