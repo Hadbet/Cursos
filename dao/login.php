ff9483f6ad123c859f05bb3c50d789ec669bad58
hadbet <hadbet.altamirano@grammer.com>
2024-06-19 10:59:13 -0600

    Correccion de nomina en el ingreso

--- dao/login.php
+++ dao/login.php
@@ -6,6 +6,28 @@ session_start();
 $Nomina = $_POST['nomina'];
 $Password = $_POST['password'];
 
+if (strlen($Nomina) == 1) {
+    $Nomina = "0000000" . $Nomina;
+}
+if (strlen($Nomina) == 2) {
+    $Nomina = "000000" . $Nomina;
+}
+if (strlen($Nomina) == 3) {
+    $Nomina = "00000" . $Nomina;
+}
+if (strlen($Nomina) == 4) {
+    $Nomina = "0000" . $Nomina;
+}
+if (strlen($Nomina) == 5) {
+    $Nomina = "000" . $Nomina;
+}
+if (strlen($Nomina) == 6) {
+    $Nomina = "00" . $Nomina;
+}
+if (strlen($Nomina) == 7) {
+    $Nomina = "0" . $Nomina;
+}
+
 $Nomina = str_pad($Nomina, 8, "0", STR_PAD_LEFT);
 $statusLogin = verificacionUsuario($Nomina, $Password);
 
