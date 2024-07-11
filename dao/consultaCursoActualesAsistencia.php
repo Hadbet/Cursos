0e07b8474e8440b3bf83bf5cfed10c930b75696b
hadbet <hadbet.altamirano@grammer.com>
2024-07-11 13:06:40 -0600

    Se separo consulta de form_asistencia.html para que sea de manera individual

--- dao/consultaCursoActualesAsistencia.php
+++ dao/consultaCursoActualesAsistencia.php
@@ -9,7 +9,7 @@ function ContadorApu()
     $con = new LocalConector();
     $conex = $con->conectar();
 
-    $datos = mysqli_query($conex, "SELECT * FROM `Cursos` WHERE `Estatus` =1 AND `Fecha` < CURDATE();;");
+    $datos = mysqli_query($conex, "SELECT * FROM `Cursos` WHERE `Estatus` =1 AND `Fecha` < CURDATE();");
 
     $resultado = mysqli_fetch_all($datos, MYSQLI_ASSOC);
     echo json_encode(array("data" => $resultado));
